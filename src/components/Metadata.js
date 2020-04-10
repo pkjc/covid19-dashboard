fd94d4611855abbef764d88ecbc236ce377cf1b6
Pankaj <mepankaj@live.in>
2020-04-10 12:22:38 -0400

    added favicons and meta tags

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-
+import favicon from "../assets/images/favicon.ico";
 const Metadata = () => {
   return (
     <Helmet>
@@ -34,73 +34,67 @@ const Metadata = () => {
       <link
         rel="apple-touch-icon"
         sizes="57x57"
-        href="../assets/images/favicons/apple-icon-57x57.png"
+        href="../assets/images/apple-icon-57x57.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="60x60"
-        href="../assets/images/favicons/apple-icon-60x60.png"
+        href="../assets/images/apple-icon-60x60.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="72x72"
-        href="../assets/images/favicons/apple-icon-72x72.png"
+        href="../assets/images/apple-icon-72x72.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
-        href="../assets/images/favicons/apple-icon-76x76.png"
+        href="../assets/images/apple-icon-76x76.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="114x114"
-        href="../assets/images/favicons/apple-icon-114x114.png"
+        href="../assets/images/apple-icon-114x114.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="120x120"
-        href="../assets/images/favicons/apple-icon-120x120.png"
+        href="../assets/images/apple-icon-120x120.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="144x144"
-        href="../assets/images/favicons/apple-icon-144x144.png"
+        href="../assets/images/apple-icon-144x144.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="152x152"
-        href="../assets/images/favicons/apple-icon-152x152.png"
+        href="../assets/images/apple-icon-152x152.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="../assets/images/favicons/apple-icon-180x180.png"
+        href="../assets/images/apple-icon-180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="../assets/images/favicons/android-icon-192x192.png"
+        href="../assets/images/android-icon-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="../assets/images/favicons/favicon-32x32.png"
+        href="../assets/images/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="../assets/images/favicons/favicon-96x96.png"
+        href="../assets/images/favicon-96x96.png"
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="../assets/images/favicons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="../assets/images/favicons/manifest.json" />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon} />
     </Helmet>
   );
 };
